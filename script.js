dff9d5ceb114fdcf46f7175370b51efb90c7ad09
Timmy <timgabrielnguyen@gmail.com>
2019-04-04 16:03:27 -0700

    Updated ToDo Count

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+  TODO_ITEM: "todo-container",
+  TODO_CHECKBOX: "todo-checkbox",
+  TODO_TEXT: "todo-text",
+  TODO_DELETE: "todo-delete"
+};
+
+const list = document.getElementById("todo-list");
+const itemCountSpan = document.getElementById("item-count");
+const uncheckedCountSpan = document.getElementById("unchecked-count");
+
+let itemCount = 0;
+let uncheckedCount = 0;
+
+function addToDo(todo) {
+  itemCount += todo;
+  itemCountSpan.innerHTML = itemCount;
 }
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+function removeToDo(todo) {
+  uncheckedCount += todo;
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+}
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  addToDo(1);
+  removeToDo(1);
+  // alert('New TODO button clicked!')
 }
